625c09c2c3416dfc2265cd3e29b2094f9d01e1ae
Deificz <99456747+Deificz@users.noreply.github.com>
2023-08-05 17:39:22 +0800

    Removed htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
   output: {
@@ -16,13 +16,15 @@ module.exports = {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
+      {
+        test: /\.ico$/,
+        loader: 'file-loader',
+        options: {
+          name: 'favicon.ico',
+        },
+      },
       
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './dist/index.html', 
-      favicon: './src/assets/images/favicon.ico', 
-    }),
-  ],
+ 
 };
